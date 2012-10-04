67d69c4c7f68daeea579637c6916d234e708f218
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2012-10-04 17:21:38 +0200

    initial import

---
+++ wallcommits/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import ListView
+from flickr.models import Photo
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', ListView.as_view(
+        model=Photo
+    ), name='home'),
+    url(r'^flickr/', include('flickr.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
